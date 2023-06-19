21fec16c03b66b384391158a4c0895956350dfab
purvi <jainpurvi20+1@gmail.com>
2023-06-19 10:02:28 +0530

    use effect - state, api data, map that data, doc title

--- src/App.js
+++ src/App.js
@@ -1,10 +1,46 @@
 import './App.css';
 import Header from './components/Header.js';
+import React, {useState, useEffect} from 'react';
 
 function App() {
+
+    const [state, setState] = useState(1);
+    const [data, setData] = useState([]);
+
+
+// useeffect is mainly used when fetching data from an api or http request
+// it gets called on component re rendering
+// [] means to give any dependency. we can pass [state]
+// if we want to call it only once then pass in a blank array as the second arguement
+
+    useEffect(() => {
+        async function getData(){
+            const get = await fetch(`https://hub.dummyapis.com/employee?noofRecords=${state}&idStarts=1001`);
+            const res = await get.json();
+            setData(res);
+        }
+
+        getData();
+        document.title = `${state} Employees`;
+    }, [state])
+
     return (
-        <div className='App'>
+        <div>
             <Header />
+            <button onClick={()=>setState(state+1)}> 
+                Click me {state}
+            </button>
+            {
+                data.map((element, index)=> {
+                    return (
+                        <div className='data' key={index}>
+                            <h4>{element.firstName}</h4>
+                            <h4>{element.lastName}</h4>
+                            <h4>{element.email}</h4>
+                        </div>
+                    )
+                })
+            }
         </div>
     );
 }
